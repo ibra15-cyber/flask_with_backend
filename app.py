c5c2faacb35a5049c050a73a44af3b937e146766
nyars nyars <iasafianu@st.ug.edu.gh>
2022-02-16 11:57:05 +0000

    after adding database from heroku

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from enum import unique
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from send_email import send_email
+from sqlalchemy import func
 
 
 app=Flask(__name__) #creating our app so our app name
 #our app connected to a database postgres database locally through sql alchemy
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:nyars150@localhost/height_collector'
+# app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:'enterpassword'@localhost/height_collector'
+#using the postgre url on heroku
+app.config['SQLALCHEMY_DATABASE_URI']='DATABASE_URL: postgres://nmianmplbaqnkn:bf436ad0145bb8cea80914d8b758d46a16b7b3e57931501f1e2a67c074e99fc4@ec2-34-206-148-196.compute-1.amazonaws.com:5432/dfac3jc3khglm7?sslmode=require'
 #making an obj of our dabase through SQLAlchemy
 db=SQLAlchemy(app)
  
@@ -36,16 +39,24 @@ def success():
     if request.method =='POST':
         email = request.form['email_name'] #get the input in form field
         height = request.form['height_name'] #get the input in form field
-        #calling our script send_email that has a method send_email to handle emailing
-        try: 
-            send_email(email,height)
-        except:
-            print(None)
-        #if the email is already in the table return the index page with a notification
+        
+        #if the email entered is already in the db table return the index page with a notification else continue
         if db.session.query(Data).filter(Data.email==email).count()== 0: #we use count to confirm if the same email is entered twice 0 is no 1 is yes
             data=Data(email, height) #passing to our class which passes it to column in our db
             db.session.add(data) #creating a session to pass data each time
             db.session.commit() #then commiting 
+
+            #after submiting count average
+            average_height=db.session.query(func.avg(Data.height)).scalar()
+            average_height=round(average_height, 1)
+            
+            #count
+            count = db.session.query(Data.height).count()
+            #calling our script send_email that has a method send_email to handle
+            try: 
+                send_email(email,height,average_height,count)
+            except:
+                print(None)
             return render_template("success.html") #we rendering the static part of our site while sending data to the db
     #if the db session isnt new ie 0 render the same page with a text
     return render_template("index.html", text="You have entered the same email before") #return the same page otherwise with a text. then we grab the text in the html
--- send_email.py
+++ send_email.py
@@ -5,21 +5,23 @@ import smtplib, ssl
 #we are creating a method that will wrap our emailing from my email account
 #to any one's that entered their height
 #our fn takes the fellows email and height
-def send_email(email, height): 
+def send_email(email, height, average_height, count): 
     from_email="nyars15@gmail.com" #my email that will do the sending
     from_passwd="who?WHAT?why?.@gmaa"
     to_email=email                  #the email to send msg to
 
     subject="Height data"           #subject
-    message="Hey there, your height is <strong>%s</strong>." %height #the message
+    message="Hey there, your height is <strong>%s</strong>.<br> The average height is <strong>%s</strong>, out of <strong>%s</strong> people.<br> Thanks!" %(height, average_height, count) #the message
 
+    #message body and format
     msg=MIMEText(message, "html") #creating a Message, MIMEText obj
     msg["Subject"] = subject #adding a subject
     msg["To"] = to_email    #adding a to field that will contain the email to send to 
     msg["From"] = from_email #addding the from field that will contain my email
 
-    gmail=smtplib.SMTP('smtp.gmail.com', port=587) #linking up my Mimetext with google servers bc my email is google
-    gmail.ehlo
-    gmail.starttls(context=ssl.create_default_context()) #this solved the error
-    gmail.login(from_email, from_passwd) #login with my email
-    gmail.send_message(msg) #gmail server should invoke send_message method with the content of the MIMEText
+    #gmail server
+    server=smtplib.SMTP('smtp.gmail.com', port=587) #linking up my Mimetext with google servers bc my email is google
+    server.ehlo
+    server.starttls(context=ssl.create_default_context()) #this solved the error
+    server.login(from_email, from_passwd) #login with my email
+    server.send_message(msg) #gmail server should invoke send_message method with the content of the MIMEText
